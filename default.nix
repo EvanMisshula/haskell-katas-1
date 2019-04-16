a3e726a849b17e5c70c8eba62d7bd368d452bfc1
Evan Misshula <emisshula@jjay.cuny.edu>
2019-04-15 23:01:11 -0400

    adds nix configuration and containers with unicode support

---
+++ default.nix
@@ -0,0 +1,20 @@
+{ mkDerivation, base, base-unicode-symbols, bytestring, containers
+, containers-unicode-symbols, hspec, mtl, QuickCheck, stdenv
+}:
+mkDerivation {
+  pname = "katas";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [ base ];
+  testHaskellDepends = [
+    base base-unicode-symbols bytestring containers
+    containers-unicode-symbols hspec mtl QuickCheck
+  ];
+  jailbreak = true;
+  doCheck = false;
+  homepage = "https://github.com/adomokos/hashmir#readme";
+  license = "unknown";
+  hydraPlatforms = stdenv.lib.platforms.none;
+}
